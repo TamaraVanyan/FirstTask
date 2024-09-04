f06b3a919cf0c352a253e87b2189b34f14601686
TamaraVanyan <tam.vanyan@gmail.com>
2024-09-05 01:07:03 +0400

    Adding homewor tasks about primitive types

--- SumOfThreeNumbers.java
+++ SumOfThreeNumbers.java
@@ -14,15 +14,13 @@ public class SumOfThreeNumbers {
 
         double sum = num1 + num2 + num3;
 
-
         int integerPart = (int) sum;
+        System.out.println("Գումարի ամբողջ մասը: " + integerPart);
 
         double DecimalPart = sum - integerPart;
+        System.out.println("Գումարի տասնորդական մասը: " + DecimalPart);
 
         int integerSum = (int) num1 + (int) num2 + (int) num3;
-
-        System.out.println("Գումարի ամբողջ մասը: " + integerPart);
-        System.out.println("Գումարի տասնորդական մասը: " + DecimalPart);
         System.out.println("Երեք թվերի ամբողջ մասերի գումարը: " + integerSum);
 
         scanner.close();
